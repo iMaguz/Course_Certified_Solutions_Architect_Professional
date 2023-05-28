01e5fe0b549fa4887da5d0f33ba3f2289a15f7a8
iMaguz <22669271+iMaguz@users.noreply.github.com>
2023-05-28 14:39:03 -0400

    modified to use stress-ng

--- lab-scaling/stress-testing-boot.sh
+++ lab-scaling/stress-testing-boot.sh
@@ -2,7 +2,5 @@
 # USAGE: Place in in the User Data section (under Advanced) when launching an EC2 instance
 # DESCRIPTION: After updating from the repo, installs stress, a tool used to create various system load for testing purposes.
 yum update -y
-wget https://dl.fedoraproject.org/pub/epel/epel-release-latest-7.noarch.rpm -P /tmp
-yum install -y /tmp/epel-release-latest-7.noarch.rpm
-yum install stress -y
-/usr/bin/stress --cpu 8 --timeout 10m
+yum install stress-ng -y
+/usr/bin/stress-ng --cpu 0 --cpu-method all --keep-name --cpu-load 100 --timeout 10m
